c398af28e8c3b440763e6de55f0fc20ace1f241c
James Logan <jameslogan@Jamess-MacBook-Pro-2.local>
2016-12-04 16:18:15 -0600

    added login/logout functionality to the application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = "Account successfully created! Welcome to Armory!"
       redirect_to @user
     else
